afc0dfc828d3584e8320e5615fe050d0d5cd29c2
Oguzhan Yilmaz <12402748@student.pxl.be>
2024-10-07 22:52:04 +0200

    Kluer 3 Hover effect:  Wanneer de gebruiker over de afbeelding hovert met de muis, dan verandert de achtergrondkleur van het venster naar de kleur van de afbeelding.

--- GIT TAAK/MainWindow.xaml.cs
+++ GIT TAAK/MainWindow.xaml.cs
@@ -13,16 +13,13 @@ namespace Git_taak1
         private void Green_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
             this.Background = new SolidColorBrush(Colors.Green);
-            ColorInfoText.Text = "Groen is de kleur van genezing";
-            ColorCodeText.Text = "#008000";  // Groen hex-code
+        
         }
 
         private void Red_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
             this.Background = new SolidColorBrush(Colors.Red);
-            ColorInfoText.Text = " Rood is de kleur van warmte";
-            ColorCodeText.Text = "#FF0000";  // Geel hex-code
-
+          
 
         }
 
@@ -30,9 +27,7 @@ namespace Git_taak1
         {
             this.Background = new SolidColorBrush(Colors.Yellow);
 
-            ColorInfoText.Text = "Geel is de kleur van levenslust";
-            ColorCodeText.Text = "#FFFF00";  // Geel hex-code
-
+           
 
         }
 
@@ -40,9 +35,7 @@ namespace Git_taak1
         {
             this.Background = new SolidColorBrush(Colors.Blue);
 
-            ColorInfoText.Text = "Blauw is de kleur van intelligentie";
-            ColorCodeText.Text = "#0000FF";  // Blauw hex-code
-
+           
         }
 
         private void Image_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
@@ -50,8 +43,7 @@ namespace Git_taak1
 
             this.Background = new SolidColorBrush(Colors.LightGray);
             //brengt de 2 Texte terug tot beginstand.
-            ColorInfoText.Text = "Klik op een kleur voor meer informatie.";
-            ColorCodeText.Text = "Klik op een kleur voor meer informatie.";  
+           
         }
 
 
