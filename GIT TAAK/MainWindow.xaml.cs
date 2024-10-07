b7de0b02acd64c081276e06b794fc1fdb0e5f12c
Oguzhan Yilmaz <12402748@student.pxl.be>
2024-10-07 22:46:21 +0200

    Afbeeldingsselectie en Informatie: dus de text verandered naar de juiste de informatie en ook de kleuren code werkt ook.  ik heb ook gedaan dat als je de muis buiten de tekst box haalt dat de kleuren code en de informatie terug tot het hoe in het begin was.

---
+++ GIT TAAK/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Git_taak1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Green_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Green);
+            ColorInfoText.Text = "Groen is de kleur van genezing";
+            ColorCodeText.Text = "#008000";  // Groen hex-code
+        }
+
+        private void Red_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Red);
+            ColorInfoText.Text = " Rood is de kleur van warmte";
+            ColorCodeText.Text = "#FF0000";  // Geel hex-code
+
+
+        }
+
+        private void Yellow_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Yellow);
+
+            ColorInfoText.Text = "Geel is de kleur van levenslust";
+            ColorCodeText.Text = "#FFFF00";  // Geel hex-code
+
+
+        }
+
+        private void Blue_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Blue);
+
+            ColorInfoText.Text = "Blauw is de kleur van intelligentie";
+            ColorCodeText.Text = "#0000FF";  // Blauw hex-code
+
+        }
+
+        private void Image_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+            this.Background = new SolidColorBrush(Colors.LightGray);
+            //brengt de 2 Texte terug tot beginstand.
+            ColorInfoText.Text = "Klik op een kleur voor meer informatie.";
+            ColorCodeText.Text = "Klik op een kleur voor meer informatie.";  
+        }
+
+
+     
+
+    }
+}
